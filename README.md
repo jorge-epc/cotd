70f17ce3cae313680f5a8aa405ac96e5b1b2cd6e
Jorge Padilla <jorge.esteban.padilla@gmail.com>
2017-08-23 14:59:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Catch of the day
+
+Final project for the [React For Beginners Course](https://reactforbeginners.com)
+
+You can preview the app [here](https://cotd-jzyrgznxih.now.sh)
---
+++ src/Base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyAtbst5PTn7bkko4DHeCFgXrTS6adNboTM",
+  authDomain: "catch-of-the-day-f0e76.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-f0e76.firebaseio.com",
+});
+
+export default base;
